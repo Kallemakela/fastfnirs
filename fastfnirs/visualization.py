c2e12ed1e4f373b3039583e88234d6acb93eec10
Kallemakela <kalle.makela@aalto.fi>
2023-10-13 12:41:58 +0300

    topo and joint plots

--- fastfnirs/visualization.py
+++ fastfnirs/visualization.py
@@ -1,4 +1,5 @@
 import mne
+import matplotlib.pyplot as plt
 
 
 def plot_evoked(epochs_dict, conditions=None):
@@ -24,3 +25,82 @@ def plot_evoked(epochs_dict, conditions=None):
         styles=styles_dict,
     )
     return fig
+
+
+def plot_topo(epochs_dict, **kwargs):
+    topomap_args = {
+        "extrapolate": "local",
+        "time_format": "%d s",
+        # 'contours': contours,
+        "cmap": "RdBu_r",
+        # 'colorbar': False,
+        "cbar_fmt": "% 2.2f",
+        "sensors": True,
+    }
+    if "vlim" in kwargs:
+        topomap_args["vlim"] = kwargs["vlim"]
+    if "times" in kwargs:
+        topomap_args["times"] = kwargs["times"]
+
+    all_epochs = mne.concatenate_epochs(list(epochs_dict.values()))
+    if "conditions" not in kwargs:
+        kwargs["conditions"] = all_epochs.metadata["trial_type"].unique()
+    if "ch_types" not in kwargs:
+        kwargs["ch_types"] = ["hbo", "hbr"]
+
+    for condition in kwargs["conditions"]:
+        for ch_type in kwargs["ch_types"]:
+            evo = (
+                all_epochs[condition]
+                .average(picks=ch_type)
+                .rename_channels(lambda x: x[:-4])
+            )
+            fig = evo.plot_topomap(
+                **topomap_args,
+                # title=f'{condition}/{ch_type}',
+                show=False,
+            )
+            fig.suptitle(f"{condition}/{ch_type}")
+            plt.show()
+
+
+def plot_joint(epochs_dict, **kwargs):
+    topomap_args = {
+        "extrapolate": "local",
+        "time_format": "%d s",
+        # 'contours': contours,
+        "cmap": "RdBu_r",
+        # 'colorbar': False,
+        "cbar_fmt": "% 2.2f",
+        "sensors": True,
+    }
+    ts_args = {
+        # 'ylim': dict(hbo=[vmin, vmax]),
+        "spatial_colors": True,
+    }
+    if "vlim" in kwargs:
+        topomap_args["vlim"] = kwargs["vlim"]
+        ts_args["ylim"] = dict(hbo=kwargs["vlim"], hbr=kwargs["vlim"])
+
+    all_epochs = mne.concatenate_epochs(list(epochs_dict.values()))
+    if "conditions" not in kwargs:
+        kwargs["conditions"] = all_epochs.metadata["trial_type"].unique()
+    if "ch_types" not in kwargs:
+        kwargs["ch_types"] = ["hbo", "hbr"]
+
+    for condition in kwargs["conditions"]:
+        for ch_type in kwargs["ch_types"]:
+            evo = (
+                all_epochs[condition]
+                .average(picks=ch_type)
+                .rename_channels(lambda x: x[:-4])
+            )
+            fig = evo.plot_joint(
+                times=kwargs["times"] if "times" in kwargs else "peaks",
+                topomap_args=topomap_args,
+                ts_args=ts_args,
+                # title=f'{condition}/{ch_type}',
+                show=False,
+            )
+            fig.suptitle(f"{condition}/{ch_type}")
+            plt.show()
